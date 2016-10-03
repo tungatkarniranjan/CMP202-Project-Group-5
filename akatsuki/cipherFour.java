2043df4ca7c3ba9b991f48e78b098ebf802437d8
tungatkarniranjan <tungatkarniranjan68@gmail.com>
2016-10-02 17:12:51 -0700

    Initial commit for cipherFour Technique
    This is an initial commit for cipherFour technique. Only the structure
    is defined.

---
+++ akatsuki/cipherFour.java
@@ -0,0 +1,53 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import java.util.*;
+import greenfoot.*;
+import java.awt.Color;
+
+/**
+ * This class will implement the cipher interface.
+ * Version - 1 - Initial Commit
+ *                     Class structure defined
+ */
+public class cipherFour extends Actor implements cipher
+{
+    /* //Hint Actor; This actor will store the hint image and will be displayed by the world method[ yet to be implemented]*/
+
+    /**
+     * Act - do whatever the cipherFour wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+    }
+
+    /**
+     * 
+     */
+    public String encrypt(String stringToencrypt)
+    {
+        return "";
+    }
+
+    /**
+     * 
+     */
+    public String decrypt(String stringTodecrypt)
+    {
+        return "";
+    }
+
+    /**
+     * 
+     */
+    public Object getHint()
+    {
+        return null;
+    }
+
+    /**
+     * 
+     */
+    public boolean checkAnswerValidity(String answer)
+    {
+        return true;
+    }
+}

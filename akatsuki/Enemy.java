c7950b07b75ea6c9afd4b15de60109f3f4aecd13
mtanwar21 <mayanktanwar1989@gmail.com>
2016-10-26 11:46:46 -0700

    Added methods in enemy class

--- akatsuki/Enemy.java
+++ akatsuki/Enemy.java
@@ -11,33 +11,67 @@ public class Enemy extends Actor implements enemyinterface
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
+     
      */
+    private static final double WALKING_SPEED = 2.0;
+    protected String state ="moveFree";
+    protected int positionX;
+    protected int positionY;
     public void act() 
     {
-        // Add your action code here.
+        moveActor();
+        turnLeft();
+        
     }    
     
     public void turnRight(){
-    
-    //turn right     
+        turn(3);
     };
     
     public void turnLeft(){
-    //turn left
+        turn(-3);
     };
     
-    public void move(){
-        //move forward
+    public void moveActor(){
+        double angle = Math.toRadians( getRotation() );
+        int x = (int) Math.round(getX() + Math.cos(angle) * WALKING_SPEED);
+        int y = (int) Math.round(getY() + Math.sin(angle) * WALKING_SPEED);
+        setLocation(x, y);
     };
     
     public void moveRandom(){
-    
-    //moverandom
+        
+        if ( Greenfoot.getRandomNumber(100) < 10 )
+        {
+            turn(Greenfoot.getRandomNumber(90)-45);
+        }
     };
     
-    public void attack(){
-        //attack
+    public void attack(int X, int Y){
+        positionX = X;
+        positionY = Y;
+        state ="attack";
+        
     };
+
+    public boolean checkActor(Class clss)
+    {
+        Actor actor = getOneObjectAtOffset(0, 0, clss);
+        return actor != null;        
+    }
+    
+    
+    public boolean atWorldEdge()
+    {
+        if(getX() < 20 || getX() > getWorld().getWidth() - 20)
+            return true;
+        if(getY() < 20 || getY() > getWorld().getHeight() - 20)
+            return true;
+        else
+            return false;
+    }
+    
+    
     
     
 }

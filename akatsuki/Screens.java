501fa45bc4a377715e290ff731e0431db676bd9b
AjayTanpure <ajaytanpure92@gmail.com>
2016-10-27 15:14:50 -0700

    Added Mapscreens and Screen java classes

---
+++ akatsuki/Screens.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Screens here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Screens extends Actor
+{
+    /**
+     * Act - do whatever the Screens wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    public void addButtons(){
+        System.out.println("");
+    }
+}

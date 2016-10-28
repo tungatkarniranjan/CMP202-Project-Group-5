0f95d9cf368e50c91282756b114b72ee187f022c
darshan009 <darshansapaliga009@gmail.com>
2016-10-27 19:31:22 -0700

    enemy tank and ammunition class

---
+++ akatsuki/Ammunition.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import greenfoot.*;
+import java.awt.Color;
+
+/**
+ * 
+ */
+public class Ammunition extends Enemy
+{
+    private Enemy enemy;
+    private int attackXpos ;
+    private int attackYpos ;
+    
+    /**
+     * Act - do whatever the Ammunition wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Ammunition(Enemy enemy ,int attackXpos,int attackYpos) {
+        this.enemy = enemy;
+        this.attackXpos =attackXpos;
+        this.attackYpos=attackYpos;
+    }
+    
+    public void act()
+    {
+        Actor city = getOneIntersectingObject(City.class);
+        if(city != null) {
+            getWorld().removeObject(this);
+        }else{
+           // Actor cityPos = (Actor)(getWorld().getObjects(City.class).get(0));  
+            turnTowards(attackXpos,attackYpos);
+            move(4);
+        }
+    }
+}

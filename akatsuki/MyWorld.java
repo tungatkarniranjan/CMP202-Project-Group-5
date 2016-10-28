484051515f137da870772b87c94e0511ef8aa769
tungatkarniranjan <tungatkarniranjan68@gmail.com>
2016-10-27 17:39:00 -0700

    MyWorld.java changed

--- akatsuki/MyWorld.java
+++ akatsuki/MyWorld.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import java.util.*;
 import greenfoot.*;
 import java.awt.Color;
@@ -12,8 +11,44 @@ public class MyWorld extends World
     /**
      * Constructor for objects of class MyWorld.
      */
+    
+    Screens homeScreen;     
+    Screens newGameScreen;
+    RandomUtitility randomUtility;
+    
     public MyWorld()
     {
-        super(600, 400, 1);
+        super(1600, 800, 1);
+        initializeHomeScreen();
+        initializeRandomUitility();
+        
     }
+    
+    public void initializeHomeScreen()
+    {
+        homeScreen = new HomeScreen();
+        addObject(homeScreen, 800, 400);
+        homeScreen.addButtons();
+    }
+    
+    
+    public void initializeNewGameScreen()
+    {
+        newGameScreen = new NewGameScreen();
+        addObject(newGameScreen, 800, 400);
+        newGameScreen.addButtons();
+    }
+    
+    public void initializeRandomUitility()
+    {
+       randomUtility = new RandomUtitility();
+      // randomUtility.initializeMapScreenRegister();
+    }
+    
+    public void initializeMapScreen(){
+        MapScreen randomMap = randomUtility.getMapScreen();
+        addObject(randomMap, 800, 400);
+        
+    }
+    
 }

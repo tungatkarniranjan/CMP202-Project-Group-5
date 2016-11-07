170e4fcf89ca5365792af16ef7e571b29c7e4165
darshan009 <darshansapaliga009@gmail.com>
2016-11-06 17:50:43 -0800

    Enemy tank modified and ammunition interface added

---
+++ akatsuki/AmmunitionInterface.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class AmmunitionInterfave here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AmmunitionInterface  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class AmmunitionInterfave
+     */
+    public AmmunitionInterface()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
+}

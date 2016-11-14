479098658964fc4c24d645b13ed0736f8c64bfe9
darshan009 <darshansapaliga009@gmail.com>
2016-11-13 17:13:01 -0800

    fire class added - called from server-side

---
+++ akatsuki/AmmunitionFire.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AmmunitionFire here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AmmunitionFire extends Enemy
+{
+    public AmmunitionFire(){
+        //make a call here when the attack sequence is initiated
+        attackNow();
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    
+    public void attackNow(){
+        //make an attack call to specific ammunition class
+    }
+}

9ba66b3ae43cbf96783c8137dcb61b2f0f8c2f4d
TheBloodMage <nachiket663@gmail.com>
2016-11-15 00:23:56 -0800

    Formatted according to the interface

--- akatsuki/CipherActorThree.java
+++ akatsuki/CipherActorThree.java
@@ -11,70 +11,50 @@ import java.awt.Color;
 
 public class CipherActorThree extends CipherActor implements cipher {
 
-    char city[];
-    String key;
+	char city[];
+	String key;
     String keySet;
     
-    CipherActorThree(String m) {
-       key = m;
-       keySet = "_abcdefghijklmnopqrstuvwxyz";
-    }
+	public cipherActorThree() {
+		super();
+		key = "key";
+		keySet = "_abcdefghijklmnopqrstuvwxyz";
+	}
+	
+	@Override
+	public String encrypt(String stringToencrypt) {
+		city = stringToencrypt.toCharArray();
+		char temp[] = new char[city.length];
+		int index;
+		for(int i =0 ; i<city.length ; i++ ){
+			index = keySet.indexOf(city[i]) + keySet.indexOf(key.charAt(i%key.length()));
+			temp[i] = keySet.charAt(index>26?index-26:index);
+		}
+		return String.valueOf(temp);
+	}
+	
+	@Override
+	public String decrypt(String stringTodecrypt) {
+		int index;
+		char temp[] = new char[stringTodecrypt.length()];
+		city = stringTodecrypt.toCharArray();
+		for(int i =0 ; i<city.length ; i++ ){
+			index = keySet.indexOf(city[i]) - keySet.indexOf(key.charAt(i%key.length()));
+			temp[i] = keySet.charAt(index<0?index+26:index);
+		}
+		//System.out.println(temp);
+		return String.valueOf(temp);
+	}
 
-    public char[] getCity() {
-        return city;
-    }
+	@Override
+	public Object getHint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    public void setCity(char[] city) {
-        this.city = city;
-    }
-    
-    public char[] encryptCity() {
-        char temp[] = new char[city.length];
-        int index;
-        for(int i =0 ; i<city.length ; i++ ){
-
-            index = keySet.indexOf(city[i]) + keySet.indexOf(key.charAt(i%key.length    ()));
-            temp[i] = keySet.charAt(index>26?index-26:index);
-        }
-        System.out.println(temp);
-        return(temp);
-    }
-    
-    public void decryptCity(char encryptedCity[]) {
-        int index;
-        char temp[] = new char[encryptedCity.length];
-        for(int i =0 ; i<city.length ; i++ ){
-            index = keySet.indexOf(encryptedCity[i]) - keySet.indexOf(key.charAt(i%key.length()));
-            temp[i] = keySet.charAt(index<0?index+26:index);
-        }
-        System.out.println(temp);
-    }
-    
-    public Object getHint(){
-        return "";
-    }
-    
-    public boolean checkAnswerValidity(String answer){
-        return true;
-    }
-
-    @Override
-    public String encrypt(String stringToencrypt) {
-        // TODO Auto-generated method stub
-        char temp[] = new char[city.length];
-        int index;
-        for(int i =0 ; i<city.length ; i++ ){
-
-            index = keySet.indexOf(city[i]) + keySet.indexOf(key.charAt(i%key.length    ()));
-            temp[i] = keySet.charAt(index>26?index-26:index);
-        }
-        System.out.println(temp);
-        return new String(temp);
-    }
-
-    @Override
-    public String decrypt(String stringTodecrypt) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public boolean checkAnswerValidity(String answer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }

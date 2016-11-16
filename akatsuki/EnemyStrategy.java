af537fe91aea4d9430b88f99bd43f2585fe2ee36
mtanwar21 <mayanktanwar1989@gmail.com>
2016-11-15 23:23:02 -0800

    EnemyStrategy file

---
+++ akatsuki/EnemyStrategy.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class EnemyStrategy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface EnemyStrategy  
+{
+    public void move(Enemy e);
+}

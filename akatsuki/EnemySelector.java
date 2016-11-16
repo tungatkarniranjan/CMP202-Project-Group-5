7eab303062a5d2f350e48e7f2465ac372b9ab384
darshan009 <darshansapaliga009@gmail.com>
2016-11-15 20:06:07 -0800

    enemy selector class

---
+++ akatsuki/EnemySelector.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class EnemySelector here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemySelector  
+{
+    
+    private ArrayList<String> enemies= new ArrayList<>();
+        
+    EnemySelector(){
+        enemies.add("Tank");
+        enemies.add("Plane");
+        enemies.add("Soldier");
+    }
+    
+    public Enemy produceEnemies(int enemyId){
+        System.out.println("Producing the enemy");
+        int counter = 0;
+        String enemyName = enemies.get(enemyId);
+        Enemy enemy;
+        if(enemyName == "Tank"){
+            enemy = new EnemyTank();
+        }else if(enemyName == "Plane"){
+            enemy = new EnemyPlane();
+        }else if(enemyName == "Soldier"){
+            enemy = new EnemySoldier();
+        }else{
+            enemy = null;
+        }
+        return enemy;
+        
+    }
+    
+}

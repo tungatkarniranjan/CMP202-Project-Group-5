c708d6da4a97ca9a6ff537b7ef151e55c8ed536b
mtanwar21 <mayanktanwar1989@gmail.com>
2016-11-15 23:26:55 -0800

    Enemy Tank with Strategy pattern

--- akatsuki/EnemyTank.java
+++ akatsuki/EnemyTank.java
@@ -12,83 +12,31 @@ public class EnemyTank extends Enemy
      * Act - do whatever the EnemyTank wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int count = 0;
+    
+
+
+    private EnemyStrategy strategy;
     public void act() 
     {
         if(state == "moveFree")
         {
-            moveActor();
-            turnAtEdge();
-            foundCity();
-            foundTank();
+            changeStrategy(new MoveFreeStrategy());
+             strategy.move(this);
         }
         else if(state == "attack")
         {
-            attack(positionX,positionY);
-        }
-    }
-    
-    public void turnRight(){
-    
-    //turn right     
-    };
-    
-    public void turnLeft(){
-    //turn left
-    };
-    
-    public void move(){
-        //move forward
-    };
-    
-    public void moveRandom(){
-    
-    //moverandom
-    };
-    
-    
-     public void turnAtEdge()
-    {
-        if ( atWorldEdge() )
-        {
-            turn(45);
-        }
-    }
-    
-    public void attack(int X, int Y)
-    {
-             System.out.println(state);  
-              moveActor();
-              turnTowards(X,Y);
-              setLocation(X-220,Y);
-              setRotation(0);
-              Actor ammunition;
-              if(this.count%70==0){
-                getWorld().addObject(new Ammunition(this,X,Y), getX(), getY());
-
-        }
-        count++;
-              System.out.println(state);
-    }
-    
-    public void foundCity()
-    {
-        if(checkActor(City.class))
-        {
-              turn(17);
-        }
-    }
-    
-     public void foundTank()
-     {
+            changeStrategy(new AttackStrategy(positionX,positionY));
+            strategy.move(this);
+        }        
+    }    
         
-         if(checkActor(EnemyTank.class))
-        {
-              turn(17);
-        }
-        
-     }
+    public void changeStrategy(EnemyStrategy s)
+    {
+       strategy = s;
+    }
     
     
+    
+   
     
 }
